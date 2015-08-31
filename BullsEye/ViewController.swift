62ecfab274abd08aef82a63c35167d61b4523d08
Steve Anavi <steve@smokio.com>
2015-08-31 15:35:27 +0200

    Start

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,17 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue = 0
+    var targetValue = 0
+    var scoreValue = 0
+    var points = 0
+    var round = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.startNewRound()
+        self.updateLabels()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(scoreValue)
+        roundLabel.text = String(round)
+    }
+    
+    func calcuateScore() {
+        let difference = abs(targetValue - currentValue)
+        points = 100 - difference
+        scoreValue += points
+        round += 1
     }
 
-
+    @IBAction func showAlert() {
+        calcuateScore()
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)" + "\nYou scored \(points) points"
+        let alert = UIAlertController(title: title,
+            message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+        startNewRound()
+        updateLabels()
+    }
+    
+    @IBAction func sliderMoved(slider: UISlider) {
+        println("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
 }
 
